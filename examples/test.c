100728a7b4b5789bd2f9ec7dd8488ad8f21aabb7
T0mEE5596 <159699718+T0mEE5596@users.noreply.github.com>
2025-02-07 15:35:26 +0330

    Updating test.c

--- examples/test.c
+++ examples/test.c
@@ -1,15 +1,18 @@
-#include<string.h>
 #include<stdio.h>
 #include "../src/multicfg.h"
 
 
 int main(int argc, char *argv[]) {
+    if (argc < 4){
+        printf("too few arguments\n"):
+        return -1;
+    }
     struct cords stc;
     int ret = multicfg_wrap(&stc, argv[1], argv[2], argv[3]);
 
     if(ret != 0) {
         printf("err\n");
-        return -1;
+        return -2;
     }
     printf("value: %s\n", stc.value);
     return 0;
